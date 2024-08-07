f61cf3a7f5fabe561615fab684f5eccac2fd5f52
Alexander  Batyushkin <akkur.software@gmail.com>
2024-08-07 20:35:24 +0600

    Создан и настроен проект

---
+++ sources/Program.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace Vktg;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Log.Logger = CreateLogger();
+        Log.Logger.Information("Vktg started");
+
+        Console.ReadKey();
+    }
+
+    static ILogger CreateLogger()
+    {
+        var logFilePath = Path.Combine(Environment.CurrentDirectory, "vktg-.log");
+        var logLevel = LogEventLevel.Verbose;
+        var logOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+
+        return new LoggerConfiguration()
+            .WriteTo.Console(logLevel, logOutputTemplate)
+            .WriteTo.File(logFilePath, logLevel, logOutputTemplate, rollingInterval: RollingInterval.Day)
+            .Enrich.FromLogContext()
+            .CreateLogger();
+    }
+}
